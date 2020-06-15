4bb22f148dbe554e0fcf6584a6e30d761df8d81f
Jayantajit Gogoi <jayanta.gogoi525@gmail.com>
2020-06-15 18:33:54 +0000

    network mo

--- scripts/clean-feeds.sh
+++ scripts/clean-feeds.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-rm -rf feeds/luci/modules/luci-mod-network
+#rm -rf feeds/luci/modules/luci-mod-network
 rm -rf feeds/luci/applications/luci-app-firewall
 rm -rf feeds/luci/applications/luci-app-shadowsocks-libev
-rm -rf feeds/luci/applications/luci-app-firewall
 rm -rf feeds/luci/applications/luci-app-vnstat
 rm -rf feeds/packages/net/shadowsocks-libev
