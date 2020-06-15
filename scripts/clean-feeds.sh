84b4bd6ed65312f17e71ae33eb898b218b3009c2
Jayantajit Gogoi <jayanta.gogoi525@gmail.com>
2020-06-15 14:03:43 +0000

    initial cache build

---
+++ scripts/clean-feeds.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm -rf feeds/luci/modules/luci-mod-network
+rm -rf feeds/luci/applications/luci-app-firewall
+rm -rf feeds/luci/applications/luci-app-shadowsocks-libev
+rm -rf feeds/luci/applications/luci-app-firewall
+rm -rf feeds/luci/applications/luci-app-vnstat
+rm -rf packages/net/shadowsocks-libev
