e938180104f772ccee461071f1dce7921bc053c2
Jayantajit Gogoi <jayanta.gogoi525@gmail.com>
2020-06-16 07:57:18 +0000

    remove mod network

--- scripts/clean-feeds.sh
+++ scripts/clean-feeds.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #rm -rf feeds/luci/modules/luci-mod-network
+rm -rf feeds/mptcp/luci-mod-network
 rm -rf feeds/luci/applications/luci-app-firewall
 rm -rf feeds/luci/applications/luci-app-shadowsocks-libev
 rm -rf feeds/luci/applications/luci-app-vnstat
