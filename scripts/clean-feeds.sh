93274fe20744f678a13a136b063011e2baeec18f
Jayantajit Gogoi <jayanta.gogoi525@gmail.com>
2020-06-22 11:48:33 +0000

    new mptcp alternative trial

--- scripts/clean-feeds.sh
+++ scripts/clean-feeds.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 #rm -rf feeds/luci/modules/luci-mod-network
 rm -rf feeds/mptcp/luci-mod-network
-rm -rf feeds/luci/applications/luci-app-firewall
-rm -rf feeds/luci/applications/luci-app-shadowsocks-libev
-rm -rf feeds/luci/applications/luci-app-vnstat
-rm -rf feeds/packages/net/shadowsocks-libev
+#rm -rf feeds/luci/applications/luci-app-firewall
+rm -rf feeds/mptcp/luci-app-firewall
+#rm -rf feeds/luci/applications/luci-app-shadowsocks-libev
+rm -rf feeds/mptcp/luci-app-shadowsocks-libev
+#rm -rf feeds/luci/applications/luci-app-vnstat
+rm -rf feeds/mptcp/luci-app-vnstat
+#rm -rf feeds/packages/net/shadowsocks-libev
+rm -rf feeds/mptcp/shadowsocks-libev
