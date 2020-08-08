11e6596c1cc4af1417db5a0dcadd1371c1b14614
Jayantajit Gogoi <jayanta.gogoi525@gmail.com>
2020-08-08 17:36:08 +0000

    pr-send

--- scripts/feeds-change.sh
+++ scripts/feeds-change.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#sed -i 's/0/1/g' feeds/packages/utils/irqbalance/files/irqbalance.config
-#sed -i "s,boardinfo.system,'Rockchip RK3328 ARMv8',g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+sed -i 's/0/1/g' feeds/packages/utils/irqbalance/files/irqbalance.config
+sed -i "s,boardinfo.system,'Rockchip RK3328 ARMv8',g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
